1e20d8d90b6fcc3d89ca5de6c79b1ec351e6d99b
Rohit Kulkarni <kulkarni.rohitva@gmail.com>
2026-01-21 20:18:05 -0800

    update run all exps

--- harp/scripts/run_all_experiments.sh
+++ harp/scripts/run_all_experiments.sh
@@ -6,12 +6,12 @@ chmod +x harp/scripts/evaluate.sh
 chmod +x harp/scripts/train.sh
 
 # get data
-harp/scripts/data.sh --config harp/config/data_config.yaml
+harp/scripts/data.sh harp/config/data_config.yaml
 
 # train all networks
-harp/scripts/train.sh --config harp/config/f_train_config.yaml
-hap/scripts/train.sh --config harp/config/csc_g_train_config.yaml
-harp/scripts/train.sh --config harp/config/harp_pi_train_config.yaml
+harp/scripts/train.sh harp/config/f_train_config.yaml
+hap/scripts/train.sh harp/config/csc_g_train_config.yaml
+harp/scripts/train.sh harp/config/harp_pi_train_config.yaml
 
 # evaluate all networks
-harp/scripts/evaluate.sh --config harp/config/evaluate_config.yaml
+harp/scripts/evaluate.sh harp/config/evaluate_config.yaml
